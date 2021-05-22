79bf9bf353bffa74475714fe8f46c18abd625cbb
ggs134 <inditow@gmail.com>
2021-05-22 22:12:42 +0900

    daejun : state.py comment updated

--- eth/vm/forks/daejun/state.py
+++ eth/vm/forks/daejun/state.py
@@ -38,8 +38,22 @@ from .validation import validate_daejun_transaction
 
 
 class DaejunTransactionExecutor(BerlinTransactionExecutor):
+
+    ## Executor() 동작 순서 ##
+
     # def build_evm_message():
     #     # 가스 차감 후 메시지 객체 생성[frontier코드 참조]
+    #     pass
+
+    # def build_computation():
+    #     # VM연산 수행[frontier코드 참조, berlin으로 상속방법 참조]
+    #     # computation : apply_create_message() or apply_message() [frontier코드 참조]
+    #     # -> apply_computaion() : loop연산 (메모리, 스택, 리턴 등), 상태 commit [base코드 참조]
+    #     pass
+
+    # def finalize_computation():
+    #     # 가스 환불(refuld)[frontier코드 참조]
+    #     # 트랜잭션 실사용 가스 계산, 마이너에게 지급(coinbase상태값에 반영)
     #     pass
 
     def build_evm_message(self, transaction: SignedTransactionAPI) -> MessageAPI:
@@ -93,17 +107,6 @@ class DaejunTransactionExecutor(BerlinTransactionExecutor):
             create_address=contract_address,
         )
         return message
-
-    # def build_computation():
-    #     # VM연산 수행[frontier코드 참조, berlin으로 상속방법 참조]
-    #     # computation : apply_create_message() or apply_message() [frontier코드 참조]
-    #     # -> apply_computaion() : loop연산 (메모리, 스택, 리턴 등), 상태 commit [base코드 참조]
-    #     pass
-
-    # def finalize_computation():
-    #     # 가스 환불(refuld)[frontier코드 참조]
-    #     # 트랜잭션 실사용 가스 계산, 마이너에게 지급(coinbase상태값에 반영)
-    #     pass
 
     def finalize_computation(self,
                              transaction: SignedTransactionAPI,
